2d020a64b419bdcb8ff1fb0b485fc15f604b5089
Pessoal <Pessoal@BOMPMTZM9354.local>
2020-05-31 02:54:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/navigation/header/header.component';
+import { DeleteUserModalComponent } from './shared/components/modals/delete-user-modal/delete-user-modal.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { ResendRegistrationTokenComponent } from './components/resend-registration-token/resend-registration-token.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DeleteUserModalComponent,
+    EditUserComponent,
+    LoginUserComponent,
+    RegisterUserComponent,
+    ResendRegistrationTokenComponent,
+    ListUserComponent,
+    WelcomeComponent
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
